8a4b2585dd37d89a11062fc75f76fa7dbc4b8ec4
Administrator <Administrator@192.168.1.10>
2016-12-14 10:10:59 +0530

    i added simple output

--- SalesTool/src/com/karan/Main.java
+++ SalesTool/src/com/karan/Main.java
@@ -5,6 +5,7 @@ public class Main
 	public static void displayGreeting()
 	{
 		System.out.println("hello karan");
+		System.out.println("you have registered successfully");
 	}
 
 	public static void main(String[] args)
