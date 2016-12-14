8abd95b9b3577c725cde43f6295044f13eaecff3
Administrator <Administrator@192.168.1.10>
2016-12-14 09:46:37 +0530

    initial checkin of sales tool

---
+++ SalesTool/src/com/karan/Main.java
@@ -0,0 +1,21 @@
+package com.karan;
+
+public class Main 
+{
+	public static void displayGreeting()
+	{
+		System.out.println("hello karan");
+	}
+
+	public static void main(String[] args)
+	{
+		SalesData obj=new SalesData();
+		displayGreeting();
+		obj.display();
+
+		
+		
+		
+	}
+
+}
---
+++ SalesTool/src/com/karan/SalesData.java
@@ -0,0 +1,12 @@
+package com.karan;
+
+public class SalesData
+{
+	int data[]={0,4,42};
+	
+	public void display()
+	{
+		System.out.println("jai shree krishna");
+	}
+
+}
